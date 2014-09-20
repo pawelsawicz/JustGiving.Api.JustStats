9e15b39c24582783a95e2870fdab4ff86f6b0a5e
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-20 15:30:16 +0100

    add new module

---
+++ JustGiving.Api.JustStats/Modules/StatisticsModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace JustGiving.Api.JustStats.Modules
+{
+    public class StatisticsModule : NancyModule
+    {
+        public StatisticsModule()
+        {
+            Get["/statistics/{name}"] = parameter =>
+            {
+                return parameter.name;
+            };
+        }
+    }
+}
