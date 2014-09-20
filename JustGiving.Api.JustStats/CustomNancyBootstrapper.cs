904eb259bcb1711cd6c8e3a6d8e29c6201ec4a8d
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-20 17:51:08 +0100

    add new convenctions

--- JustGiving.Api.JustStats/CustomNancyBootstrapper.cs
+++ JustGiving.Api.JustStats/CustomNancyBootstrapper.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Nancy;
+using Nancy.Conventions;
 
 namespace JustGiving.Api.JustStats
 {
     public class CustomNancyBootstrapper : DefaultNancyBootstrapper
     {
-
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts",
+                @"Scripts"));
+            base.ConfigureConventions(nancyConventions);
+        }
     }
 }
